dff690f0940bd1e5eac6dfd0afa2c37c943ca8bf
Kumar Harsh <kumar.harsh492@gmail.com>
2022-06-26 11:45:30 +0530

    Added the c# demo code

---
+++ CSharpdemo/CSharpdemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int score = 100;
+            int sc2ore2;
+        }
+    }
+}
